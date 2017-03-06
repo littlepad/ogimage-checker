18360c614752e5d8e474458a0e62cec7a795e87f
littlepad <littlepad@gmail.com>
2017-03-06 22:58:58 +0900

    promiseで書き直した

--- js/checker.js
+++ js/checker.js
@@ -1,22 +1,25 @@
 const client = require('cheerio-httpcli');
 
-// callbackを指定しなかったのでPromiseオブジェクトが返る
-const p = client.fetch('https://google.co.jp/');
-p.then((result) => {
-  // レスポンスヘッダを参照
-  // console.log(result.response.headers);
 
-  // HTMLタイトルを表示
-  console.log(result.$('title').eq(0).text());
+function getMeta(url) {
+  return new Promise((resolve, reject) => {
+    client.fetch(url).then((result) => {
+      // レスポンスヘッダを参照
+      // console.log(result.response.headers);
 
-  // og:imgを表示
-  console.log(result.$('meta[property="og:image"]').attr('content'));
-});
+      // HTMLタイトルを表示
+      console.log(result.$('title').eq(0).text());
 
-p.catch((err) => {
-  console.log(err);
-});
+      // og:imgを表示
+      console.log(result.$('meta[property="og:image"]').attr('content'));
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+}
 
-p.finally(() => {
-  console.log('done');
-});
+getMeta('https://google.co.jp/')
+  .then(() => {
+    console.log('done');
+  }
+);
