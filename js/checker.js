b4a1a95e2d818eb45eadff19ba5f12e326e74612
littlepad <littlepad@gmail.com>
2017-03-07 00:10:20 +0900

    promiseの連続処理

--- js/checker.js
+++ js/checker.js
@@ -1,25 +1,36 @@
 const client = require('cheerio-httpcli');
 
+const URL_LIST = [
+  'http://www.yahoo.co.jp/',
+  'https://google.co.jp/',
+  'http://www.apple.com/jp/',
+];
 
 function getMeta(url) {
   return new Promise((resolve, reject) => {
     client.fetch(url).then((result) => {
-      // レスポンスヘッダを参照
-      // console.log(result.response.headers);
-
-      // HTMLタイトルを表示
-      console.log(result.$('title').eq(0).text());
-
-      // og:imgを表示
-      console.log(result.$('meta[property="og:image"]').attr('content'));
+      resolve(result);
     }).catch((err) => {
-      console.log(err);
+      reject(err);
     });
   });
 }
 
-getMeta('https://google.co.jp/')
-  .then(() => {
-    console.log('done');
-  }
-);
+function outputResult(results) {
+  results.map((result) => {
+    // HTMLタイトルを表示
+    console.log(result.$('title').eq(0).text());
+    // URLを表示
+    console.log(result.response.request.href);
+    // og:imgを表示
+    console.log(result.$('meta[property="og:image"]').attr('content'));
+  });
+}
+
+Promise.all(URL_LIST.map(url => getMeta(url)))
+  .then((results) => {
+    outputResult(results);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
