425573d3d8233b4414a216a057958ab319c77102
littlepad <littlepad@gmail.com>
2020-03-20 20:08:57 +0900

    yarn run lint --fix

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const client = require('cheerio-httpcli');
 const bodyParser = require('body-parser');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
